3f6e3c91b7a9969173de5458193564ef122c22ff
sunyanfen <sunyanfen@xcloudgame.com>
2019-07-18 17:52:48 +0800

    基本目录okk

---
+++ src/router/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import '../css/base.css';
+
+class IndexRouter extends Component {
+    render() {
+        return (
+            <div className='wrapper'>
+                this is router page
+            </div>
+        )
+    }
+}
+export default IndexRouter
